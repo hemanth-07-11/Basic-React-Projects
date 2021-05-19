1d44edd2f73ecf34815755d16f4b7dc125f25dbf
Hemanth N <hemanthnov2001@gmail.com>
2021-05-19 23:59:20 +0530

    Added source files for 'display name' sample proj

---
+++ Display name/src/data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+      id: 1,
+      name: 'Hemanth',
+      age: 20,
+      image:
+        'https://avatars.githubusercontent.com/u/65853034?v=4',
+    },
+    {
+      id: 2,
+      name: 'Person 2',
+      age: 22,
+      image:
+        'https://st.depositphotos.com/2101611/3925/v/950/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg',
+    },
+    {
+      id: 3,
+      name: 'person 3',
+      age: 19,
+      image:
+        'https://st2.depositphotos.com/1006318/5909/v/950/depositphotos_59095205-stock-illustration-businessman-profile-icon.jpg',
+    },
+    {
+      id: 4,
+      name: 'Person 4',
+      age: 25,
+      image:
+        'https://st3.depositphotos.com/19428878/36349/v/1600/depositphotos_363493744-stock-illustration-unknown-male-avatar-profile-image.jpg',
+    },
+    {
+      id: 5,
+      name: 'Person 5',
+      age: 29,
+      image:
+        'https://st4.depositphotos.com/36844638/38157/v/1600/depositphotos_381577612-stock-illustration-business-man-avatar-illustration-circle.jpg',
+    },
+  ];
