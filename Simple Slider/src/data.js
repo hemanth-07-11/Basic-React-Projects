5c6bf615ef5d867b25bffcde4ee394e5f0245b7d
Hemanth N <hemanthnov2001@gmail.com>
2021-05-20 00:38:08 +0530

    Added Source files for Simple Slider.

---
+++ Simple Slider/src/data.js
@@ -0,0 +1,40 @@
+const people = [
+    {
+      id: 2,
+      image:
+        'https://allworldpm.com/wp-content/uploads/2016/10/230x230-avatar-dummy-profile-pic.jpg',
+      name: 'Person 2',
+      title: 'Office manager',
+      quote:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales felis mi, vel molestie augue sagittis id. In semper ut diam varius ultricies. Donec bibendum molestie bibendum. Integer at accumsan sapien, non dapibus leo. Praesent ornare iaculis iaculis. Pellentesque accumsan iaculis neque eget vehicula. Mauris non elit consequat, auctor elit et, tempor enim. Donec porttitor massa a urna maximus porta. Proin eget cursus dui.',
+    },
+    {
+      id: 1,
+      image:
+        'https://avatars.githubusercontent.com/u/65853034?v=4',
+      name: 'HEMANTH N',
+      title: 'C E O ',
+      quote:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales felis mi, vel molestie augue sagittis id. In semper ut diam varius ultricies. Donec bibendum molestie bibendum. Integer at accumsan sapien, non dapibus leo. Praesent ornare iaculis iaculis. Pellentesque accumsan iaculis neque eget vehicula. Mauris non elit consequat, auctor elit et, tempor enim. Donec porttitor massa a urna maximus porta. Proin eget cursus dui.',
+    },
+    {
+      id: 3,
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaYWtEYVDfkaSDmsoETH5F8bktbEAWfrC5YT9ZmsaK_gX1iipmZmTH0n-JhlTp_CbQYxw&usqp=CAU',
+      name: 'Person 3',
+      title: 'Designer',
+      quote:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales felis mi, vel molestie augue sagittis id. In semper ut diam varius ultricies. Donec bibendum molestie bibendum. Integer at accumsan sapien, non dapibus leo. Praesent ornare iaculis iaculis. Pellentesque accumsan iaculis neque eget vehicula. Mauris non elit consequat, auctor elit et, tempor enim. Donec porttitor massa a urna maximus porta. Proin eget cursus dui.',
+    },
+    {
+      id: 4,
+      image:
+        'https://storage.googleapis.com/stateless-campfire-pictures/2019/04/6f56fc81-dummyuserimage-1556273962c8p4l.jpg',
+      name: 'Person 4',
+      title: 'Backend Dev',
+      quote:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales felis mi, vel molestie augue sagittis id. In semper ut diam varius ultricies. Donec bibendum molestie bibendum. Integer at accumsan sapien, non dapibus leo. Praesent ornare iaculis iaculis. Pellentesque accumsan iaculis neque eget vehicula. Mauris non elit consequat, auctor elit et, tempor enim. Donec porttitor massa a urna maximus porta. Proin eget cursus dui.',
+    },
+  ];
+  
+  export default people;
