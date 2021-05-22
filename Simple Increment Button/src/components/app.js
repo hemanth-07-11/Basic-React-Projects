99840036111a1aa2e1e9952a37b49ef4f79fb3f8
Hemanth N <hemanthnov2001@gmail.com>
2021-05-22 18:48:46 +0530

    Added Source files for the challenge

---
+++ Simple Increment Button/src/components/app.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import IncrementButton from './increment-button';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
+
+    increment() {
+        this.setState({count: this.state.count + 1});
+    }
+
+    render() {
+        return (
+            <div className="parent">
+                <h1> Simple Increment Button - REACT CHALLENGE</h1>
+                <div className="count">
+                    Number of clicks: {this.state.count}
+                </div>
+                <IncrementButton onIncrementClick={() => this.increment()} />
+            </div>
+        );
+    }
+}
