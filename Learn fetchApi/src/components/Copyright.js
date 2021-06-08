e19382969e0f43561911b5686da6019075404b58
Hemanth N <hemanthnov2001@gmail.com>
2021-06-08 22:37:43 +0530

    Added source files for the project

---
+++ Learn fetchApi/src/components/Copyright.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Copyright = () => {
+    return (
+        <div className="copyright">
+            <span>Designed By: </span>
+            <span className="color">Hemanth N</span>
+        </div>
+    )
+}
+
+export default Copyright
