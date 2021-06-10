3105659e83f1fc9094d436ba6f0608b0a0a2305d
Hemanth N <hemanthnov2001@gmail.com>
2021-06-11 00:05:24 +0530

    Added source files for the challenge

---
+++ List Users challenge/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+import './styles/userList.css';
+
+import Header from './components/Header';
+import UserList from './containers/UserList';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Header appName="SUPER DATE"/>
+                <UserList />
+            </div>
+        );
+    }
+}
+
+export default App;
