c7a3c798fe91e5e5f07b916785e07e1a1effa4c9
Hemanth N <hemanthnov2001@gmail.com>
2021-06-12 13:11:18 +0530

    Added source files for accordion (fcc)

---
+++ Accordion ( FCC )/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './App.css';
+import data from './data';
+import SingleQuestion from './questions';
+
+function App() {
+
+  const [questions, setQuestions] = useState(data)
+
+  return (
+    <div className="app">
+      <div className="container">
+        <h3>Questions and Answers about Cyber Security</h3>
+          <section className='info'>
+            {
+              questions.map( (question) => {
+                return (
+                  <SingleQuestion key={question.id} {...question}></SingleQuestion>
+                )
+              })
+            }
+          </section>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Accordion ( FCC )/src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+    {
+      id: 1,
+      title: 'What is the purpose of this website?',
+      info:
+        'This is a simple website to create awareness about cyber security and also to implement few concepts learnt in react.',
+    },
+    {
+      id: 2,
+      title: 'What is cyber Security?',
+      info:
+        'Computer security, cybersecurity or information technology security is the protection of computer systems and networks from information disclosure, theft of or damage to their hardware, software, or electronic data, as well as from the disruption or misdirection of the services they provide.',
+    },
+    {
+      id: 3,
+      title: 'What Are the Costs of a Cyber Attack?',
+      info:
+        'With all the news stories in recent years about big organizations and data breaches, we have a general idea of what cyber attacks cost. Money and reputations are lost, which impacts the survival of a business. CEOs have had to step down from their jobs on several occasions. But there are hidden costs to an attack that tend to emerge later when dealing with the incident. For one, mission critical data and technology might be disrupted during the recovery process, which halts important progress at work. And the longer that your system is remedying its compliancy, the costs of an attack become steeper. In the end, keeping your networks secure is about the continuation of daily operations.  ',
+    },
+    {
+      id: 4,
+      title: 'What are the differences among the various malware types: virus, worm, trojan, bot/botnet, etc.?',
+      info:
+        'Although most people understand what spamware is without a formal definition, the most common malware types (viruses, worms, trojans, and bots) are frequently confused by computer experts (who are not cybersecurity experts) and computer noobs like.        ',
+    },
+    {
+      id: 5,
+      title: 'Why is cyber threat remediation?',
+      info:
+        'In the context of cyber threats remediation refers to reversing or stopping the damage caused by cyber attacks. Compare with cyber threat mitigation, which refers to a less effective counter measure. ',
+    },
+  ]
+  export default questions
