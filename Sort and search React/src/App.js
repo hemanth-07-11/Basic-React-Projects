5016f14a204b2c266d11cec00b2d47b9799aba2b
Hemanth N <hemanthnov2001@gmail.com>
2021-06-24 19:57:52 +0530

    Added files for the challenge

---
+++ Sort and search React/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Table } from './table/table';
+
+function App() {
+  return (
+    <div>
+      <h1 id='title'>React Table - Mockoroo API - Sorting and Searching </h1>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
